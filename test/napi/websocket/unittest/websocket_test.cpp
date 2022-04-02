90c34e81c273493712e93e8baecde4dd8e299953
maosiping <maosiping@huawei.com>
2022-04-02 18:11:25 +0800

    unittest
    Signed-off-by: maosiping <maosiping@huawei.com>

--- test/napi/websocket/unittest/websocket_test.cpp
+++ test/napi/websocket/unittest/websocket_test.cpp
@@ -17,7 +17,7 @@
 
 #include "gtest/gtest.h"
 
-#include "netstack_log.h"
+#include "websocket_async_work.h"
 #include "websocket_exec.h"
 
 namespace OHOS::NetStack {
@@ -38,13 +38,13 @@ void WebsocketTest::SetUp() {}
 
 void WebsocketTest::TearDown() {}
 
-HWTEST_F(WebsocketTest, TestExecConnect1, testing::ext::TestSize.Level1)
+HWTEST_F(WebsocketTest, TestExecConnect1, testing::ext::TestSize.Level4)
 {
     bool ret = WebSocketExec::ExecConnect(nullptr);
     ASSERT_FALSE(ret);
 }
 
-HWTEST_F(WebsocketTest, TestExecConnect2, testing::ext::TestSize.Level1)
+HWTEST_F(WebsocketTest, TestExecConnect2, testing::ext::TestSize.Level4)
 {
     auto context = new ConnectContext(nullptr, nullptr);
     bool ret = WebSocketExec::ExecConnect(context);
@@ -52,13 +52,49 @@ HWTEST_F(WebsocketTest, TestExecConnect2, testing::ext::TestSize.Level1)
     ASSERT_FALSE(ret);
 }
 
-HWTEST_F(WebsocketTest, TestExecClose1, testing::ext::TestSize.Level1)
+HWTEST_F(WebsocketTest, TestExecConnect3, testing::ext::TestSize.Level4)
+{
+    auto context = new ConnectContext(nullptr, nullptr);
+    WebSocketAsyncWork::ExecConnect(nullptr, context);
+    bool ret = context->IsExecOK();
+    delete context;
+    ASSERT_FALSE(ret);
+}
+
+HWTEST_F(WebsocketTest, TestExecConnect4, testing::ext::TestSize.Level4)
+{
+    auto context = new ConnectContext(nullptr, nullptr);
+    WebSocketAsyncWork::ExecConnect(nullptr, context);
+    bool ret = context->IsParseOK();
+    delete context;
+    ASSERT_FALSE(ret);
+}
+
+HWTEST_F(WebsocketTest, TestExecConnect5, testing::ext::TestSize.Level4)
+{
+    auto context = new ConnectContext(nullptr, nullptr);
+    WebSocketAsyncWork::ExecConnect(nullptr, context);
+    int32_t ret = context->GetErrorCode();
+    delete context;
+    ASSERT_EQ(ret, -1);
+}
+
+HWTEST_F(WebsocketTest, TestExecConnect6, testing::ext::TestSize.Level4)
+{
+    auto context = new ConnectContext(nullptr, nullptr);
+    ASSERT_FALSE(WebSocketExec::ExecConnect(context));
+    int32_t ret = context->GetErrorCode();
+    delete context;
+    ASSERT_EQ(ret, 0);
+}
+
+HWTEST_F(WebsocketTest, TestExecClose1, testing::ext::TestSize.Level4)
 {
     bool ret = WebSocketExec::ExecClose(nullptr);
     ASSERT_FALSE(ret);
 }
 
-HWTEST_F(WebsocketTest, TestExecClose2, testing::ext::TestSize.Level1)
+HWTEST_F(WebsocketTest, TestExecClose2, testing::ext::TestSize.Level4)
 {
     auto context = new CloseContext(nullptr, nullptr);
     bool ret = WebSocketExec::ExecClose(context);
@@ -66,18 +102,90 @@ HWTEST_F(WebsocketTest, TestExecClose2, testing::ext::TestSize.Level1)
     ASSERT_FALSE(ret);
 }
 
-HWTEST_F(WebsocketTest, TestExecSend1, testing::ext::TestSize.Level1)
+HWTEST_F(WebsocketTest, TestExecClose3, testing::ext::TestSize.Level4)
+{
+    auto context = new CloseContext(nullptr, nullptr);
+    WebSocketAsyncWork::ExecClose(nullptr, context);
+    bool ret = context->IsExecOK();
+    delete context;
+    ASSERT_FALSE(ret);
+}
+
+HWTEST_F(WebsocketTest, TestExecClose4, testing::ext::TestSize.Level4)
+{
+    auto context = new CloseContext(nullptr, nullptr);
+    WebSocketAsyncWork::ExecClose(nullptr, context);
+    bool ret = context->IsParseOK();
+    delete context;
+    ASSERT_FALSE(ret);
+}
+
+HWTEST_F(WebsocketTest, TestExecClose5, testing::ext::TestSize.Level4)
+{
+    auto context = new CloseContext(nullptr, nullptr);
+    WebSocketAsyncWork::ExecClose(nullptr, context);
+    int32_t ret = context->GetErrorCode();
+    delete context;
+    ASSERT_EQ(ret, -1);
+}
+
+HWTEST_F(WebsocketTest, TestExecClose6, testing::ext::TestSize.Level4)
+{
+    auto context = new CloseContext(nullptr, nullptr);
+    ASSERT_FALSE(WebSocketExec::ExecClose(context));
+    int32_t ret = context->GetErrorCode();
+    delete context;
+    ASSERT_EQ(ret, 0);
+}
+
+HWTEST_F(WebsocketTest, TestExecSend1, testing::ext::TestSize.Level4)
 {
     bool ret = WebSocketExec::ExecSend(nullptr);
     ASSERT_FALSE(ret);
 }
 
-HWTEST_F(WebsocketTest, TestExecSend2, testing::ext::TestSize.Level1)
+HWTEST_F(WebsocketTest, TestExecSend2, testing::ext::TestSize.Level4)
 {
     auto context = new SendContext(nullptr, nullptr);
     bool ret = WebSocketExec::ExecSend(context);
     delete context;
     ASSERT_FALSE(ret);
+}
+
+HWTEST_F(WebsocketTest, TestExecSend3, testing::ext::TestSize.Level4)
+{
+    auto context = new SendContext(nullptr, nullptr);
+    WebSocketAsyncWork::ExecSend(nullptr, context);
+    bool ret = context->IsExecOK();
+    delete context;
+    ASSERT_FALSE(ret);
+}
+
+HWTEST_F(WebsocketTest, TestExecSend4, testing::ext::TestSize.Level4)
+{
+    auto context = new SendContext(nullptr, nullptr);
+    WebSocketAsyncWork::ExecSend(nullptr, context);
+    bool ret = context->IsParseOK();
+    delete context;
+    ASSERT_FALSE(ret);
+}
+
+HWTEST_F(WebsocketTest, TestExecSend5, testing::ext::TestSize.Level4)
+{
+    auto context = new SendContext(nullptr, nullptr);
+    WebSocketAsyncWork::ExecSend(nullptr, context);
+    int32_t ret = context->GetErrorCode();
+    delete context;
+    ASSERT_EQ(ret, -1);
+}
+
+HWTEST_F(WebsocketTest, TestExecSend6, testing::ext::TestSize.Level4)
+{
+    auto context = new SendContext(nullptr, nullptr);
+    ASSERT_FALSE(WebSocketExec::ExecSend(context));
+    int32_t ret = context->GetErrorCode();
+    delete context;
+    ASSERT_EQ(ret, 0);
 }
 
 } // namespace OHOS::NetStack
