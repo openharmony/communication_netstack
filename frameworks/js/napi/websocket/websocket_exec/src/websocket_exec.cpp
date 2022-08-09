983c958e2cb02a0afd7c6000144d788b20a1e01c
Zheng Yongjun <2660599479@qq.com>
2022-08-09 21:01:06 +0800

    Fix a potential memleak on error path
    Signed-off-by: Zheng Yongjun <2660599479@qq.com>

--- frameworks/js/napi/websocket/websocket_exec/src/websocket_exec.cpp
+++ frameworks/js/napi/websocket/websocket_exec/src/websocket_exec.cpp
@@ -716,6 +716,7 @@ void WebSocketExec::OnMessage(EventManager *manager, void *data, size_t length, 
         return;
     }
     if (memcpy_s(para->data, length, data, length) < 0) {
+        free(para->data);
         delete para;
         NETSTACK_LOGE("mem copy failed");
         return;
