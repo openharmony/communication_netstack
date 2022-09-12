f0e11d4b05d0bb8ec61115fcfad9451cd7e1add9
zhuwenchao <zhuwenchao@huawei.com>
2022-09-12 17:57:19 +0800

    ide supports http
    Signed-off-by: zhuwenchao <zhuwenchao@huawei.com>

--- frameworks/js/napi/http/cache/lru_cache/include/lru_cache_disk_handler.h
+++ frameworks/js/napi/http/cache/lru_cache/include/lru_cache_disk_handler.h
@@ -20,7 +20,6 @@
 
 #include "disk_handler.h"
 #include "lru_cache.h"
-#include "nocopyable.h"
 
 static constexpr const int MAX_DISK_CACHE_SIZE = 1024 * 1024 * 10;
 static constexpr const int MIN_DISK_CACHE_SIZE = 1024 * 1024;
