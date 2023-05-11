60d377cb7255989737c8905a29da596b6cc9f36b
yejl2326 <596324346@qq.com>
2023-05-11 09:25:44 +0800

    code-check
    Signed-off-by: yejl2326 <596324346@qq.com>

--- frameworks/js/napi/http/http_exec/src/http_exec.cpp
+++ frameworks/js/napi/http/http_exec/src/http_exec.cpp
@@ -379,7 +379,7 @@ void HttpExec::RunThread()
         SendRequest();
         ReadResponse();
         std::this_thread::sleep_for(std::chrono::milliseconds(CURL_TIMEOUT_MS));
-        static std::mutex m;
+        std::mutex m;
         std::unique_lock l(m);
         auto &infoQueue = staticVariable_.infoQueue;
         auto &contextMap = staticVariable_.contextMap;
