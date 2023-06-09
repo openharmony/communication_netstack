58efb661b43fb04ae1215d2044e590dedd1377d7
Yangys <yangyousheng@huawei.com>
2023-06-09 07:28:57 +0000

    update frameworks/js/napi/http/http_exec/src/http_exec.cpp.
    Signed-off-by: Yangys <yangyousheng@huawei.com>

--- frameworks/js/napi/http/http_exec/src/http_exec.cpp
+++ frameworks/js/napi/http/http_exec/src/http_exec.cpp
@@ -551,7 +551,7 @@ bool HttpExec::SetOption(CURL *curl, RequestContext *context, struct curl_slist 
         NETSTACK_CURL_EASY_SET_OPTION(curl, CURLOPT_POSTFIELDSIZE, context->options.GetBody().size(), context);
     }
 
-    NETSTACK_CURL_EASY_SET_OPTION(curl, CURLOPT_FORBID_REUSE, 1L, context);
+    NETSTACK_CURL_EASY_SET_OPTION(curl, CURLOPT_FORBID_REUSE, 0L, context);
 
     NETSTACK_CURL_EASY_SET_OPTION(curl, CURLOPT_XFERINFOFUNCTION, ProgressCallback, context);
     NETSTACK_CURL_EASY_SET_OPTION(curl, CURLOPT_XFERINFODATA, context, context);
