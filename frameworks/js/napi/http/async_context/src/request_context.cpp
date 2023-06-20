f49dc906fd3d7cc55e34f555155bdb7922a2c84f
Yangys <yangyousheng@huawei.com>
2023-06-20 12:32:48 +0000

    update frameworks/js/napi/http/async_context/src/request_context.cpp.
    Signed-off-by: Yangys <yangyousheng@huawei.com>

--- frameworks/js/napi/http/async_context/src/request_context.cpp
+++ frameworks/js/napi/http/async_context/src/request_context.cpp
@@ -445,7 +445,7 @@ std::string RequestContext::GetErrorMessage() const
     if (BaseContext::IsPermissionDenied()) {
         return PERMISSION_DENIED_MSG;
     }
-    if(err == HTTP_UNKNOWN_OTHER_ERROR) {
+    if (err == HTTP_UNKNOWN_OTHER_ERROR) {
         err -= HTTP_ERROR_CODE_BASE;
     }
 
