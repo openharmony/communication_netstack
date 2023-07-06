b51566442608730f737fe54d0e4febd5aa451781
xujie <xujie223@huawei.com>
2023-07-06 13:44:38 +0800

    限制客户端的最大连接数
    Signed-off-by: xujie <xujie223@huawei.com>

--- frameworks/js/napi/socket/socket_exec/src/socket_exec.cpp
+++ frameworks/js/napi/socket/socket_exec/src/socket_exec.cpp
@@ -53,6 +53,8 @@ static constexpr const int USER_LIMIT = 511;
 
 static constexpr const int ERR_SYS_BASE = 2303100;
 
+static constexpr const int MAX_CLIENTS = 1024;
+
 static constexpr const char *TCP_SOCKET_CONNECTION = "TCPSocketConnection";
 
 static constexpr const char *TCP_SERVER_ACCEPT_RECV_DATA = "TCPServerAcceptRecvData";
@@ -1455,6 +1457,14 @@ static void AcceptRecvData(int sock, sockaddr *addr, socklen_t addrLen, const Tc
         int32_t connectFD = accept(sock, reinterpret_cast<sockaddr *>(&clientAddress), &clientAddrLength);
         if (connectFD < 0) {
             continue;
+        }
+        {
+            std::lock_guard<std::mutex> lock(g_mutex);
+            if (g_clientFDs.size() >= MAX_CLIENTS) {
+                NETSTACK_LOGE("Maximum number of clients reached, connection rejected");
+                close(connectFD);
+                continue;
+            }
         }
         NETSTACK_LOGI("Server accept new client SUCCESS, fd = %{public}d", connectFD);
         {
