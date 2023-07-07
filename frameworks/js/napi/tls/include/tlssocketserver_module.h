04960600d0b87c2c5f284f3efc1d0ded881e5ac5
xujie <xujie223@huawei.com>
2023-07-07 12:29:47 +0800

    fix code format
    Signed-off-by: xujie <xujie223@huawei.com>

--- frameworks/js/napi/tls/include/tlssocketserver_module.h
+++ frameworks/js/napi/tls/include/tlssocketserver_module.h
@@ -25,52 +25,52 @@ class TLSSocketServerModuleExports {
 public:
     class TLSSocketServer {
     public:
-		static constexpr const char *FUNCTION_LISTEN = "listen";
-		static constexpr const char *FUNCTION_GET_STATE = "getState";
-		static constexpr const char *FUNCTION_SET_EXTRA_OPTIONS = "setExtraOptions";
-		static constexpr const char *FUNCTION_ON = "on";
-		static constexpr const char *FUNCTION_OFF = "off";
-		static constexpr const char *FUNCTION_GET_CERTIFICATE = "getCertificate";
-		static constexpr const char *FUNCTION_GET_PROTOCOL = "getProtocol";
+        static constexpr const char *FUNCTION_LISTEN = "listen";
+        static constexpr const char *FUNCTION_GET_STATE = "getState";
+        static constexpr const char *FUNCTION_SET_EXTRA_OPTIONS = "setExtraOptions";
+        static constexpr const char *FUNCTION_ON = "on";
+        static constexpr const char *FUNCTION_OFF = "off";
+        static constexpr const char *FUNCTION_GET_CERTIFICATE = "getCertificate";
+        static constexpr const char *FUNCTION_GET_PROTOCOL = "getProtocol";
 
         static napi_value GetCertificate(napi_env env, napi_callback_info info);
         static napi_value GetProtocol(napi_env env, napi_callback_info info);
-		static napi_value Listen(napi_env env, napi_callback_info info);
-		static napi_value GetState(napi_env env, napi_callback_info info);
-		static napi_value SetExtraOptions(napi_env env, napi_callback_info info);
-		static napi_value On(napi_env env, napi_callback_info info);
-		static napi_value Off(napi_env env, napi_callback_info info);
+        static napi_value Listen(napi_env env, napi_callback_info info);
+        static napi_value GetState(napi_env env, napi_callback_info info);
+        static napi_value SetExtraOptions(napi_env env, napi_callback_info info);
+        static napi_value On(napi_env env, napi_callback_info info);
+        static napi_value Off(napi_env env, napi_callback_info info);
     };
 
-	class TLSSocketConnection {
-	public:
-		static constexpr const char *PROPERTY_CLIENT_ID = "clientId";
-		static constexpr const char *FUNCTION_SEND = "send";
-		static constexpr const char *FUNCTION_CLOSE = "close";
-		static constexpr const char *FUNCTION_GET_REMOTE_ADDRESS = "getRemoteAddress";
-		static constexpr const char *FUNCTION_GET_REMOTE_CERTIFICATE = "getRemoteCertificate";
-		static constexpr const char *FUNCTION_GET_CERTIFICATE = "getCertificate";
-		static constexpr const char *FUNCTION_GET_CIPHER_SUITE = "getCipherSuite";
-		static constexpr const char *FUNCTION_GET_SIGNATURE_ALGORITHMS = "getSignatureAlgorithms";
-		static constexpr const char *FUNCTION_ON = "on";
-		static constexpr const char *FUNCTION_OFF = "off";
+    class TLSSocketConnection {
+    public:
+        static constexpr const char *PROPERTY_CLIENT_ID = "clientId";
+        static constexpr const char *FUNCTION_SEND = "send";
+        static constexpr const char *FUNCTION_CLOSE = "close";
+        static constexpr const char *FUNCTION_GET_REMOTE_ADDRESS = "getRemoteAddress";
+        static constexpr const char *FUNCTION_GET_REMOTE_CERTIFICATE = "getRemoteCertificate";
+        static constexpr const char *FUNCTION_GET_CERTIFICATE = "getCertificate";
+        static constexpr const char *FUNCTION_GET_CIPHER_SUITE = "getCipherSuite";
+        static constexpr const char *FUNCTION_GET_SIGNATURE_ALGORITHMS = "getSignatureAlgorithms";
+        static constexpr const char *FUNCTION_ON = "on";
+        static constexpr const char *FUNCTION_OFF = "off";
 
-		static napi_value Send(napi_env env, napi_callback_info info);
-		static napi_value Close(napi_env env, napi_callback_info info);
-		static napi_value GetRemoteAddress(napi_env env, napi_callback_info info);
-		static napi_value GetRemoteCertificate(napi_env env, napi_callback_info info);
-		static napi_value GetCipherSuites(napi_env env, napi_callback_info info);
-		static napi_value GetSignatureAlgorithms(napi_env env, napi_callback_info info);
-		static napi_value On(napi_env env, napi_callback_info info);
-		static napi_value Off(napi_env env, napi_callback_info info);
-		static napi_value GetCertificate(napi_env env, napi_callback_info info);
-	};
+        static napi_value Send(napi_env env, napi_callback_info info);
+        static napi_value Close(napi_env env, napi_callback_info info);
+        static napi_value GetRemoteAddress(napi_env env, napi_callback_info info);
+        static napi_value GetRemoteCertificate(napi_env env, napi_callback_info info);
+        static napi_value GetCipherSuites(napi_env env, napi_callback_info info);
+        static napi_value GetSignatureAlgorithms(napi_env env, napi_callback_info info);
+        static napi_value On(napi_env env, napi_callback_info info);
+        static napi_value Off(napi_env env, napi_callback_info info);
+        static napi_value GetCertificate(napi_env env, napi_callback_info info);
+    };
 
 public:
     static constexpr const char *INTERFACE_TLS_SOCKET_SERVER = "TLSSocketServer";
     static constexpr const char *FUNCTION_CONSTRUCTOR_TLS_SOCKET_SERVER_INSTANCE = "constructTLSSocketServerInstance";
     static constexpr const char *INTERFACE_PROTOCOL = "Protocol";
-	static constexpr const char *INTERFACE_TLS_SOCKET_SERVER_CONNECTION = "TLSSocketServerConnection";
+    static constexpr const char *INTERFACE_TLS_SOCKET_SERVER_CONNECTION = "TLSSocketServerConnection";
 
     static napi_value InitTLSSocketServerModule(napi_env env, napi_value exports);
 
@@ -79,7 +79,7 @@ private:
     static void DefineTLSSocketServerClass(napi_env env, napi_value exports);
     static void InitTLSSocketServerProperties(napi_env env, napi_value exports);
     static void InitProtocol(napi_env env, napi_value exports);
-	static void DefineTLSSocketConnectionClass(napi_env env, napi_value exports);
+    static void DefineTLSSocketConnectionClass(napi_env env, napi_value exports);
 };
 } // namespace TlsSocketServer
 } // namespace NetStack
