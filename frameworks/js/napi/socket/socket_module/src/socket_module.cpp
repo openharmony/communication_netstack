66c1aca0330889f54d71410a778da5f72b763f67
xujie <xujie223@huawei.com>
2023-07-07 15:44:54 +0800

    edit socket_modules.cpp add tlsserver init
    Signed-off-by: xujie <xujie223@huawei.com>

--- frameworks/js/napi/socket/socket_module/src/socket_module.cpp
+++ frameworks/js/napi/socket/socket_module/src/socket_module.cpp
@@ -40,6 +40,7 @@
 #include "tcp_extra_context.h"
 #include "tcp_send_context.h"
 #include "tlssocket_module.h"
+#include "tlssocketserver_module.h"
 #include "udp_extra_context.h"
 #include "udp_send_context.h"
 
@@ -147,6 +148,7 @@ static bool MakeUdpSocket(napi_env env, napi_value thisVal, BindContext *context
 napi_value SocketModuleExports::InitSocketModule(napi_env env, napi_value exports)
 {
     TlsSocket::TLSSocketModuleExports::InitTLSSocketModule(env, exports);
+    TlsSocketServer::TLSSocketServerModuleExports::InitTLSSocketServerModule(env, exports);
     DefineUDPSocketClass(env, exports);
     DefineTCPServerSocketClass(env, exports);
     DefineTCPSocketClass(env, exports);
