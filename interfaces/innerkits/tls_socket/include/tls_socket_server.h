e7d8be837694d3a8dfb7afe5fc33ec0fe60dc620
xujie <xujie223@huawei.com>
2023-07-13 19:20:37 +0800

    fix
    Signed-off-by: xujie <xujie223@huawei.com>

--- interfaces/innerkits/tls_socket/include/tls_socket_server.h
+++ interfaces/innerkits/tls_socket/include/tls_socket_server.h
@@ -91,7 +91,7 @@ public:
     TLSSocketServer &operator=(TLSSocketServer &&) = delete;
 
     TLSSocketServer() = default;
-    ~TLSSocketServer();
+    ~TLSSocketServer() ;
 
     /**
      * Create sockets, bind and listen waiting for clients to connect
@@ -205,6 +205,7 @@ public:
 public:
     class Connection : public std::enable_shared_from_this<Connection> {
     public:
+
         ~Connection();
         /**
          * Establish an encrypted accept on the specified socket
@@ -439,9 +440,8 @@ private:
     void InitPollList(int &listendFd);
 
     struct pollfd fds_[USER_LIMIT + 1];
-
+     
     bool isRunning_;
-
 public:
     std::shared_ptr<Connection> GetConnectionByClientID(int clientid);
 };
