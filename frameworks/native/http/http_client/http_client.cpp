a8af8d09208a0e7a6e39f2f2932d2fb50f093845
xujie <xujie223@huawei.com>
2023-10-19 10:44:32 +0800

    fix http crash Signed-off-by: xujie <xujie223@huawei.com>

--- frameworks/native/http/http_client/http_client.cpp
+++ frameworks/native/http/http_client/http_client.cpp
@@ -38,6 +38,11 @@ static constexpr int CURL_MAX_WAIT_MSECS = 10;
 static constexpr int CURL_TIMEOUT_MS = 50;
 static constexpr int CONDITION_TIMEOUT_S = 3600;
 
+HttpSession::HttpSession()
+{
+    Init();
+}
+
 HttpSession::~HttpSession()
 {
     Deinit();
