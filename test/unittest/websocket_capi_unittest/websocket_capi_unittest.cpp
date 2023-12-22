64251d772b3c96315472dac6d3790ad4348e2b1f
liuxiyao223 <liuxiyao223@huawei.com>
2023-12-22 11:27:11 +0800

    add websocket code
    Signed-off-by: liuxiyao223 <liuxiyao223@huawei.com>

--- test/unittest/websocket_capi_unittest/websocket_capi_unittest.cpp
+++ test/unittest/websocket_capi_unittest/websocket_capi_unittest.cpp
@@ -23,7 +23,7 @@
 
 #include "net_websocket.h"
 
-class WebsocketTest : public testing::Test {
+class WebSocketTest : public testing::Test {
 public:
     static void SetUpTestCase() {}
 
@@ -37,77 +37,77 @@ public:
 namespace {
 using namespace testing::ext;
 
-static void OnOpen(struct OH_NetStack_WebsocketClient *client, OH_NetStack_WebsocketClient_OpenResult openResult) {}
+static void OnOpen(struct WebSocket *client, WebSocket_OpenResult openResult) {}
 
-static void OnMessage(struct OH_NetStack_WebsocketClient *client, char *data, uint32_t length) {}
+static void OnMessage(struct WebSocket *client, char *data, uint32_t length) {}
 
-static void OnError(struct OH_NetStack_WebsocketClient *client, OH_NetStack_WebsocketClient_ErrorResult error) {}
+static void OnError(struct WebSocket *client, WebSocket_ErrorResult error) {}
 
-static void OnClose(struct OH_NetStack_WebsocketClient *client, OH_NetStack_WebsocketClient_CloseResult closeResult) {}
+static void OnClose(struct WebSocket *client, WebSocket_CloseResult closeResult) {}
 
-HWTEST_F(WebsocketTest, WebsocketConstruct001, TestSize.Level1)
+HWTEST_F(WebSocketTest, WebSocketConstruct001, TestSize.Level1)
 {
     int ret;
-    struct OH_NetStack_WebsocketClient *client = new OH_NetStack_WebsocketClient();
+    struct WebSocket *client = new WebSocket();
     const char *url1 = "www.baidu.com";
-    struct OH_NetStack_WebsocketClient_Slist header1;
+    struct WebSocket_Header header1;
     header1.fieldName = "Content-Type";
     header1.fieldValue = "application/json";
     header1.next = nullptr;
-    client = OH_NetStack_WebsocketClient_Construct(OnOpen, OnMessage, OnError, OnClose);
-    ret = OH_NetStack_WebSocketClient_AddHeader(client, header1);
-    OH_NetStack_WebSocketClient_Connect(client, url1, client->requestOptions);
+    client = OH_WebSocketClient_Constructor(OnOpen, OnMessage, OnError, OnClose);
+    ret = OH_WebSocketClient_AddHeader(client, header1);
+    OH_WebSocketClient_Connect(client, url1, client->requestOptions);
     EXPECT_EQ(ret, 0);
 }
 
-HWTEST_F(WebsocketTest, WebsocketAddHeader002, TestSize.Level1)
+HWTEST_F(WebSocketTest, WebSocketAddHeader002, TestSize.Level1)
 {
     int ret;
-    struct OH_NetStack_WebsocketClient *client = nullptr;
-    struct OH_NetStack_WebsocketClient_Slist header1;
+    struct WebSocket *client = nullptr;
+    struct WebSocket_Header header1;
     header1.fieldName = "Content-Type";
     header1.fieldValue = "application/json";
     header1.next = nullptr;
-    ret = OH_NetStack_WebSocketClient_AddHeader(client, header1);
+    ret = OH_WebSocketClient_AddHeader(client, header1);
     EXPECT_EQ(ret, 1001);
 }
 
-HWTEST_F(WebsocketTest, WebsocketConnect003, TestSize.Level1)
+HWTEST_F(WebSocketTest, WebSocketConnect003, TestSize.Level1)
 {
     int ret;
-    struct OH_NetStack_WebsocketClient *client = new OH_NetStack_WebsocketClient();
+    struct WebSocket *client = new WebSocket();
     const char *url1 = "www.baidu.com";
-    ret = OH_NetStack_WebSocketClient_Connect(client, url1, client->requestOptions);
+    ret = OH_WebSocketClient_Connect(client, url1, client->requestOptions);
 
     EXPECT_EQ(ret, 1002);
 }
 
-HWTEST_F(WebsocketTest, WebsocketSend004, TestSize.Level1)
+HWTEST_F(WebSocketTest, WebSocketSend004, TestSize.Level1)
 {
     int ret;
-    struct OH_NetStack_WebsocketClient *client = nullptr;
+    struct WebSocket *client = nullptr;
     const char *Senddata = "Hello, I love China!";
     int SendLength = std::strlen(Senddata);
-    ret = OH_NetStack_WebSocketClient_Send(client, const_cast<char *>(Senddata), SendLength);
+    ret = OH_WebSocketClient_Send(client, const_cast<char *>(Senddata), SendLength);
     EXPECT_EQ(ret, 1001);
 }
 
-HWTEST_F(WebsocketTest, WebsocketClose005, TestSize.Level1)
+HWTEST_F(WebSocketTest, WebSocketClose005, TestSize.Level1)
 {
     int ret;
-    struct OH_NetStack_WebsocketClient *client = nullptr;
-    struct OH_NetStack_WebsocketClient_CloseOption CloseOption;
+    struct WebSocket *client = nullptr;
+    struct WebSocket_CloseOption CloseOption;
     CloseOption.code = 1000;
     CloseOption.reason = " ";
-    ret = OH_NetStack_WebSocketClient_Close(client, CloseOption);
+    ret = OH_WebSocketClient_Close(client, CloseOption);
     EXPECT_EQ(ret, 1001);
 }
 
-HWTEST_F(WebsocketTest, WebsocketDestroy006, TestSize.Level1)
+HWTEST_F(WebSocketTest, WebSocketDestroy006, TestSize.Level1)
 {
     int ret;
-    struct OH_NetStack_WebsocketClient *client = new OH_NetStack_WebsocketClient();
-    ret = OH_NetStack_WebsocketClient_Destroy(client);
+    struct WebSocket *client = new WebSocket();
+    ret = OH_WebSocketClient_Destroy(client);
     EXPECT_EQ(ret, 1002);
 }
 
--- test/unittest/websocket_inner_unittest/websocket_inner_unittest.cpp
+++ test/unittest/websocket_inner_unittest/websocket_inner_unittest.cpp
@@ -23,7 +23,7 @@
 
 #include "websocket_client_innerapi.h"
 
-class WebsocketTest : public testing::Test {
+class WebSocketTest : public testing::Test {
 public:
     static void SetUpTestCase() {}
 
@@ -36,59 +36,59 @@ public:
 
 namespace {
 using namespace testing::ext;
-using namespace OHOS::NetStack::WebsocketClient;
+using namespace OHOS::NetStack::WebSocketClient;
 
 OpenOptions openOptions;
 
-static void OnMessage(WebsocketClient *client, const std::string &data, size_t length) {}
+static void OnMessage(WebSocketClient *client, const std::string &data, size_t length) {}
 
-static void OnOpen(WebsocketClient *client, OpenResult openResult) {}
+static void OnOpen(WebSocketClient *client, OpenResult openResult) {}
 
-static void OnError(WebsocketClient *client, ErrorResult error) {}
+static void OnError(WebSocketClient *client, ErrorResult error) {}
 
-static void OnClose(WebsocketClient *client, CloseResult closeResult) {}
+static void OnClose(WebSocketClient *client, CloseResult closeResult) {}
 
-HWTEST_F(WebsocketTest, WebsocketRegistcallback001, TestSize.Level1)
+HWTEST_F(WebSocketTest, WebSocketRegistcallback001, TestSize.Level1)
 {
     openOptions.headers["Content-Type"] = "application/json";
     openOptions.headers["Authorization"] = "Bearer your_token_here";
-    WebsocketClient *client = new WebsocketClient();
+    WebSocketClient *client = new WebSocketClient();
     client->Registcallback(OnOpen, OnMessage, OnError, OnClose);
     int ret = client->Connect("www.baidu.com", openOptions);
     EXPECT_EQ(ret, 0);
 }
 
-HWTEST_F(WebsocketTest, WebsocketConnect002, TestSize.Level1)
+HWTEST_F(WebSocketTest, WebSocketConnect002, TestSize.Level1)
 {
-    WebsocketClient *client = new WebsocketClient();
+    WebSocketClient *client = new WebSocketClient();
     int ret = client->Connect("www.baidu.com", openOptions);
     EXPECT_EQ(ret, 0);
 }
 
-HWTEST_F(WebsocketTest, WebsocketSend003, TestSize.Level1)
+HWTEST_F(WebSocketTest, WebSocketSend003, TestSize.Level1)
 {
     int ret;
-    WebsocketClient *client = new WebsocketClient();
+    WebSocketClient *client = new WebSocketClient();
     const char *data = "Hello, world!";
     int length = std::strlen(data);
     ret = client->Send(const_cast<char *>(data), length);
     EXPECT_EQ(ret, 0);
 }
 
-HWTEST_F(WebsocketTest, WebsocketClose004, TestSize.Level1)
+HWTEST_F(WebSocketTest, WebSocketClose004, TestSize.Level1)
 {
     CloseOption CloseOptions;
     CloseOptions.code = LWS_CLOSE_STATUS_NORMAL;
     CloseOptions.reason = "";
-    WebsocketClient *client = new WebsocketClient();
+    WebSocketClient *client = new WebSocketClient();
     int ret = client->Close(CloseOptions);
     EXPECT_EQ(ret, 1017);
 }
 
-HWTEST_F(WebsocketTest, WebsocketDestroy005, TestSize.Level1)
+HWTEST_F(WebSocketTest, WebSocketDestroy005, TestSize.Level1)
 {
     int ret;
-    WebsocketClient *client = new WebsocketClient();
+    WebSocketClient *client = new WebSocketClient();
     ret = client->Destroy();
     EXPECT_EQ(ret, 1018);
 }
