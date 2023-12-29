88e5cc588029a7ae16d62424f03a510a21ae5bb9
liuxiyao223 <liuxiyao223@huawei.com>
2023-12-29 16:02:44 +0800

    add websocket code
    Signed-off-by: liuxiyao223 <liuxiyao223@huawei.com>

--- test/unittest/websocket_inner_unittest/websocket_inner_unittest.cpp
+++ test/unittest/websocket_inner_unittest/websocket_inner_unittest.cpp
@@ -15,11 +15,10 @@
 
 #include "netstack_log.h"
 #include "gtest/gtest.h"
+#include <csignal>
 #include <cstring>
 #include <functional>
 #include <iostream>
-#include <csignal>
-#include <cstring>
 
 #include "websocket_client_innerapi.h"
 
@@ -59,13 +58,13 @@ HWTEST_F(WebSocketTest, WebSocketRegistcallback001, TestSize.Level1)
     closeOptions.code = LWS_CLOSE_STATUS_NORMAL;
     closeOptions.reason = "";
     client->Registcallback(OnOpen, OnMessage, OnError, OnClose);
-    int ret = client->Connect("www.baidu.com", openOptions);
+    int32_t ret = client->Connect("www.baidu.com", openOptions);
     EXPECT_EQ(ret, 0);
 }
 
 HWTEST_F(WebSocketTest, WebSocketConnect002, TestSize.Level1)
 {
-    int ret = 0;
+    int32_t ret = 0;
     openOptions.headers["Content-Type"] = "application/json";
     openOptions.headers["Authorization"] = "Bearer your_token_here";
     client->Registcallback(OnOpen, OnMessage, OnError, OnClose);
@@ -75,9 +74,9 @@ HWTEST_F(WebSocketTest, WebSocketConnect002, TestSize.Level1)
 
 HWTEST_F(WebSocketTest, WebSocketSend003, TestSize.Level1)
 {
-    int ret;
+    int32_t ret;
     const char *data = "Hello, world!";
-    int length = std::strlen(data);
+    int32_t length = std::strlen(data);
     client->Connect("www.baidu.com", openOptions);
     ret = client->Send(const_cast<char *>(data), length);
     EXPECT_EQ(ret, 0);
@@ -85,16 +84,17 @@ HWTEST_F(WebSocketTest, WebSocketSend003, TestSize.Level1)
 
 HWTEST_F(WebSocketTest, WebSocketClose004, TestSize.Level1)
 {
+    const int32_t WEBSOCKET_NO_CONNECTION = 1017;
     CloseOption CloseOptions;
     CloseOptions.code = LWS_CLOSE_STATUS_NORMAL;
     CloseOptions.reason = "";
-    int ret = client->Close(CloseOptions);
-    EXPECT_EQ(ret, 1017);
+    int32_t ret = client->Close(CloseOptions);
+    EXPECT_EQ(ret, WEBSOCKET_NO_CONNECTION);
 }
 
 HWTEST_F(WebSocketTest, WebSocketDestroy005, TestSize.Level1)
 {
-    int ret;
+    int32_t ret;
     ret = client->Destroy();
     delete client;
     EXPECT_EQ(ret, 0);
