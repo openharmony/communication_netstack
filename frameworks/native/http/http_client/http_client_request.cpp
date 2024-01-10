fd370f66ce7c654ba1cc96abe67102bd40c9f156
Aurora <liuxiyao223@huawei.com>
2024-01-10 13:18:33 +0000

    添加MaxLimit接口
    Signed-off-by: Aurora <liuxiyao223@huawei.com>

--- frameworks/native/http/http_client/http_client_request.cpp
+++ frameworks/native/http/http_client/http_client_request.cpp
@@ -29,6 +29,7 @@ namespace HttpClient {
 static constexpr const uint32_t HTTP_MIN_PRIORITY = 1;
 static constexpr const uint32_t HTTP_DEFAULT_PRIORITY = 500;
 static constexpr const uint32_t HTTP_MAX_PRIORITY = 1000;
+static constexpr uint32_t DEFAULT_MAX_LIMIT = 20 * 1024 * 1024;
 
 HttpClientRequest::HttpClientRequest()
     : method_(HttpConstant::HTTP_METHOD_GET),
@@ -36,7 +37,8 @@ HttpClientRequest::HttpClientRequest()
       connectTimeout_(HttpConstant::DEFAULT_CONNECT_TIMEOUT),
       protocol_(HttpProtocol::HTTP_NONE),
       proxyType_(HttpProxyType::NOT_USE),
-      priority_(HTTP_DEFAULT_PRIORITY)
+      priority_(HTTP_DEFAULT_PRIORITY),
+      maxLimit_(DEFAULT_MAX_LIMIT)
 {
 #ifndef WINDOWS_PLATFORM
     caPath_ = HttpConstant::HTTP_DEFAULT_CA_PATH;
@@ -118,6 +120,11 @@ void HttpClientRequest::SetPriority(unsigned int priority)
     priority_ = priority;
 }
 
+void HttpClientRequest::SetMaxLimit(unsigned int maxLimit)
+{
+    maxLimit_ = maxLimit;
+}
+
 const std::string &HttpClientRequest::GetURL() const
 {
     return url_;
@@ -166,6 +173,11 @@ const std::string &HttpClientRequest::GetCaPath()
 uint32_t HttpClientRequest::GetPriority() const
 {
     return priority_;
+}
+
+unsigned int HttpClientRequest::GetMaxLimit() const
+{
+    return maxLimit_;
 }
 
 void HttpClientRequest::SetHttpProxy(const HttpProxy &proxy)
