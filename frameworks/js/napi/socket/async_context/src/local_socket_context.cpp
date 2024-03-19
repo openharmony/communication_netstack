68222846036f211d5b8d5a91d62e9d9b418c3a33
liuxiyao223 <liuxiyao223@huawei.com>
2024-03-19 14:31:26 +0800

    fix bug with invalid listening events
    Signed-off-by: liuxiyao223 <liuxiyao223@huawei.com>

--- frameworks/js/napi/socket/async_context/src/local_socket_context.cpp
+++ frameworks/js/napi/socket/async_context/src/local_socket_context.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2023-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -35,8 +35,9 @@ void LocalSocketBaseContext::SetSocketFd(int sock)
     if (manager_ == nullptr) {
         return;
     }
-    LocalSocketManager *pMgr = reinterpret_cast<LocalSocketManager *>(manager_->GetData());
-    pMgr->sockfd_ = sock;
+    if (auto pMgr = reinterpret_cast<LocalSocketManager *>(manager_->GetData()); pMgr != nullptr) {
+        pMgr->sockfd_ = sock;
+    }
 }
 
 int32_t LocalSocketBaseContext::GetErrorCode() const
