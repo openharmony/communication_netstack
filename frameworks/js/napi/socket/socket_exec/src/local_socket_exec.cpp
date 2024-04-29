5400dd3716d3ffbe73040dc5fb951b8142dcd897
liuxiyao223 <liuxiyao223@huawei.com>
2024-04-29 21:46:59 +0800

    ios or macos do not support epoll
    Signed-off-by: liuxiyao223 <liuxiyao223@huawei.com>

--- frameworks/js/napi/socket/socket_exec/src/local_socket_exec.cpp
+++ frameworks/js/napi/socket/socket_exec/src/local_socket_exec.cpp
@@ -588,7 +588,7 @@ static void LocalSocketServerAccept(LocalSocketServerManager *mgr, const LocalSo
     pthread_setname_np(LOCAL_SOCKET_SERVER_ACCEPT_RECV_DATA);
 #else
     pthread_setname_np(pthread_self(), LOCAL_SOCKET_SERVER_ACCEPT_RECV_DATA);
-#endif
+
     struct sockaddr_un clientAddress;
     socklen_t clientAddrLength = sizeof(clientAddress);
     if (mgr->RegisterEpollEvent(mgr->sockfd_, EPOLLIN) == -1) {
@@ -622,6 +622,7 @@ static void LocalSocketServerAccept(LocalSocketServerManager *mgr, const LocalSo
         }
     }
     mgr->NotifyLoopFinished();
+#endif
 }
 
 static int UpdateRecvBuffer(int sock, int &bufferSize, std::unique_ptr<char[]> &buf,
