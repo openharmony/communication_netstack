2aade3dc6c0a2b6d845ee1b1ba662b39260f709b
zhangqing <zhangqing50@h-partners.com>
2024-04-30 10:24:53 +0800

    arkui-x update master OH to weekly_20240415 and OH fix build bug
    Signed-off-by: zhangqing <zhangqing50@h-partners.com>
    Change-Id: I5141b6350b00aed96fe34386fe9025559269482a

--- frameworks/js/napi/socket/socket_exec/src/local_socket_exec.cpp
+++ frameworks/js/napi/socket/socket_exec/src/local_socket_exec.cpp
@@ -30,6 +30,10 @@
 #include "socket_async_work.h"
 #include "socket_module.h"
 
+#ifndef EPOLLIN
+#define EPOLLIN 0x001
+#endif
+
 namespace {
 constexpr int BACKLOG = 32;
 
@@ -585,7 +589,7 @@ static void LocalSocketServerAccept(LocalSocketServerManager *mgr, const LocalSo
     pthread_setname_np(LOCAL_SOCKET_SERVER_ACCEPT_RECV_DATA);
 #else
     pthread_setname_np(pthread_self(), LOCAL_SOCKET_SERVER_ACCEPT_RECV_DATA);
-#endif
+
     struct sockaddr_un clientAddress;
     socklen_t clientAddrLength = sizeof(clientAddress);
     if (mgr->RegisterEpollEvent(mgr->sockfd_, EPOLLIN) == -1) {
@@ -619,6 +623,7 @@ static void LocalSocketServerAccept(LocalSocketServerManager *mgr, const LocalSo
         }
     }
     mgr->NotifyLoopFinished();
+#endif
 }
 
 static int UpdateRecvBuffer(int sock, int &bufferSize, std::unique_ptr<char[]> &buf,
