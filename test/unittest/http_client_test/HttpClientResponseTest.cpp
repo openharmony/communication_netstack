e3c2ff7426cb2ee3eaa026c62c00333b03437aff
zhaohui91 <zhaohui91@h-partners.com>
2024-09-25 12:00:33 +0800

    modify copyright
    Signed-off-by: zhaohui91 <zhaohui91@h-partners.com>

--- test/unittest/http_client_test/HttpClientResponseTest.cpp
+++ test/unittest/http_client_test/HttpClientResponseTest.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -19,7 +19,6 @@
 #include "http_client_constant.h"
 #include "netstack_log.h"
 
-#define private public
 #include "http_client_response.h"
 
 using namespace OHOS::NetStack::HttpClient;
--- test/unittest/http_client_test/HttpClientTaskTest.cpp
+++ test/unittest/http_client_test/HttpClientTaskTest.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -21,7 +21,6 @@
 #include "netstack_log.h"
 #include "netstack_common_utils.h"
 
-#define private public
 #include "http_client_task.h"
 #include "http_client.h"
 #include "http_client_error.h"
