8ad0e7e0d21f2cd034135323a5f24d1cdcf4f87e
zhengsiwen960323 <zhengsiwen2@huawei.com>
2025-05-20 08:02:28 +0000

    update test/unittest/websocket_capi_unittest/websocket_capi_unittest.cpp.
    Signed-off-by: zhengsiwen960323 <zhengsiwen2@huawei.com>

--- test/unittest/websocket_capi_unittest/websocket_capi_unittest.cpp
+++ test/unittest/websocket_capi_unittest/websocket_capi_unittest.cpp
@@ -139,7 +139,7 @@ HWTEST_F(WebSocketTest, WebSocketSendTest007, TestSize.Level1)
     ret = OH_WebSocketClient_Send(client, const_cast<char *>(senddata2), sizeof(senddata2));
     EXPECT_EQ(ret, WebSocket_ErrCode::WEBSOCKET_OK);
     ret = OH_WebSocketClient_Destroy(client);
-    EXPECT_EQ(ret, WebSocket_ErrCode::WEBSOCKET_OK);
+    EXPECT_EQ(ret, WebSocket_ErrCode::WEBSOCKET_ERROR_HAVE_NO_CONNECT_CONTEXT);
 }
 
 } // namespace
