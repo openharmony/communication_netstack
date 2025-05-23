caf646d94048e275e6b8bc3407b6f282de15fa36
liuleimin_hw <liuleimin1@huawei.com>
2025-05-23 15:37:21 +0800

    update
    Signed-off-by: liuleimin_hw <liuleimin1@huawei.com>

--- interfaces/kits/c/common/netstack_hash_map.cpp
+++ interfaces/kits/c/common/netstack_hash_map.cpp
@@ -135,166 +135,166 @@ uint32_t Netstack_PutMapEntry(Netstack_HashMap *map, const char *key, void *valu
     Netstack_HashMapEntry *ptr = map->entries[idx];
     while (ptr != nullptr) {
         // replace exist entry
-       if (strcmp(ptr->key, key) == 0) {
-           ptr->value = value;
-           return RESULT_OK;
-       }
-       ptr = ptr->next;
-   }
+        if (strcmp(ptr->key, key) == 0) {
+            ptr->value = value;
+            return RESULT_OK;
+        }
+        ptr = ptr->next;
+    }
 
-   // insert after first entry
-   Netstack_HashMapEntry *entry = (Netstack_HashMapEntry *)malloc(sizeof(Netstack_HashMapEntry));
-   if (entry == nullptr) {
-       return OUT_OF_MEMORY;
-   }
-   entry->key = strdup(key);
-   if (entry->key == nullptr) {
-       free(entry);
-       return OUT_OF_MEMORY;
-   }
-   entry->value = value;
-   entry->next = map->entries[idx]->next;
-   map->entries[idx]->next = entry;
+    // insert after first entry
+    Netstack_HashMapEntry *entry = (Netstack_HashMapEntry *)malloc(sizeof(Netstack_HashMapEntry));
+    if (entry == nullptr) {
+        return OUT_OF_MEMORY;
+    }
+    entry->key = strdup(key);
+    if (entry->key == nullptr) {
+        free(entry);
+        return OUT_OF_MEMORY;
+    }
+    entry->value = value;
+    entry->next = map->entries[idx]->next;
+    map->entries[idx]->next = entry;
 
-   return RESULT_OK;
+    return RESULT_OK;
 }
 
 void *Netstack_GetMapEntry(Netstack_HashMap *map, const char *key)
 {
-   if (NetstackInvalidMap(map) || key == nullptr) {
-       return nullptr;
-   }
+    if (NetstackInvalidMap(map) || key == nullptr) {
+        return nullptr;
+    }
 
-   uint32_t idx = Netstack_Hash(key, map->capacity);
-   Netstack_HashMapEntry *entry = map->entries[idx];
-   while (entry != nullptr) {
-       if (strcmp(entry->key, key) == 0) {
-           return entry->value;
-       }
-       entry = entry->next;
-   }
-   return nullptr;
+    uint32_t idx = Netstack_Hash(key, map->capacity);
+    Netstack_HashMapEntry *entry = map->entries[idx];
+    while (entry != nullptr) {
+        if (strcmp(entry->key, key) == 0) {
+            return entry->value;
+        }
+        entry = entry->next;
+    }
+    return nullptr;
 }
 
 uint32_t Netstack_DeleteMapEntry(Netstack_HashMap *map, const char *key)
 {
-   if (NetstackInvalidMap(map) || key == nullptr) {
-       return PARAMETER_ERROR;
-   }
+    if (NetstackInvalidMap(map) || key == nullptr) {
+        return PARAMETER_ERROR;
+    }
 
-   uint32_t idx = Netstack_Hash(key, map->capacity);
-   if (map->entries[idx] == nullptr) {
-       return RESULT_OK;
-   }
+    uint32_t idx = Netstack_Hash(key, map->capacity);
+    if (map->entries[idx] == nullptr) {
+        return RESULT_OK;
+    }
 
-   if (strcmp(map->entries[idx]->key, key) == 0) {
-       Netstack_HashMapEntry *entry = map->entries[idx];
-       map->entries[idx] = map->entries[idx]->next;
-       free(entry->key);
-       free(entry);
-       if (map->entries[idx] == nullptr) {
-           map->size--;
-       }
-       return RESULT_OK;
-   }
+    if (strcmp(map->entries[idx]->key, key) == 0) {
+        Netstack_HashMapEntry *entry = map->entries[idx];
+        map->entries[idx] = map->entries[idx]->next;
+        free(entry->key);
+        free(entry);
+        if (map->entries[idx] == nullptr) {
+            map->size--;
+        }
+        return RESULT_OK;
+    }
 
-   Netstack_HashMapEntry *prev = map->entries[idx];
-   Netstack_HashMapEntry *entry = map->entries[idx]->next;
-   while (entry != nullptr) {
-       if (strcmp(entry->key, key) == 0) {
-           prev->next = entry->next;
-           free(entry->key);
-           free(entry);
-           return RESULT_OK;
-       }
-       prev = entry;
-       entry = entry->next;
-   }
-   return RESULT_OK;
+    Netstack_HashMapEntry *prev = map->entries[idx];
+    Netstack_HashMapEntry *entry = map->entries[idx]->next;
+    while (entry != nullptr) {
+        if (strcmp(entry->key, key) == 0) {
+            prev->next = entry->next;
+            free(entry->key);
+            free(entry);
+            return RESULT_OK;
+        }
+        prev = entry;
+        entry = entry->next;
+    }
+    return RESULT_OK;
 }
 
 void Netstack_DestroyMapWithValue(Netstack_HashMap *map, Netstack_DestroyValueFunction destroyFunction)
 {
-   if (NetstackInvalidMap(map)) {
-       return;
-   }
-   Netstack_HashMapEntry *entry;
-   Netstack_HashMapEntry *next;
-   for (uint32_t i = 0; i < map->capacity; i++) {
-       entry = map->entries[i];
-       while (entry != nullptr) {
-           next = entry->next;
-           entry->next = nullptr;
-           free(entry->key);
-           entry->key = nullptr;
-           if (destroyFunction != nullptr) {
-               destroyFunction(entry->value);
-               entry->value = nullptr;
-           }
-           free(entry);
-           map->entries[i] = nullptr;
-           entry = next;
-       }
-   }
-   free(map->entries);
-   map->entries = nullptr;
-   free(map);
+    if (NetstackInvalidMap(map)) {
+        return;
+    }
+    Netstack_HashMapEntry *entry;
+    Netstack_HashMapEntry *next;
+    for (uint32_t i = 0; i < map->capacity; i++) {
+        entry = map->entries[i];
+        while (entry != nullptr) {
+            next = entry->next;
+            entry->next = nullptr;
+            free(entry->key);
+            entry->key = nullptr;
+            if (destroyFunction != nullptr) {
+                destroyFunction(entry->value);
+                entry->value = nullptr;
+            }
+            free(entry);
+            map->entries[i] = nullptr;
+            entry = next;
+        }
+    }
+    free(map->entries);
+    map->entries = nullptr;
+    free(map);
 }
 
 void Netstack_DestroyMap(Netstack_HashMap *map)
 {
-   Netstack_DestroyMapWithValue(map, nullptr);
+    Netstack_DestroyMapWithValue(map, nullptr);
 }
 
 Netstack_MapIterator *Netstack_CreateMapIterator(Netstack_HashMap *map)
 {
-   if (NetstackInvalidMap(map)) {
-       NETSTACK_LOGE("create map iterator failed: invalid map");
-       return nullptr;
-   }
-   Netstack_MapIterator *iterator = (Netstack_MapIterator *)malloc(sizeof(Netstack_MapIterator));
-   if (iterator == nullptr) {
-       NETSTACK_LOGE("failed to alloc memory for map iterator");
-       return nullptr;
-   }
-   iterator->map = map;
-   for (uint32_t i = 0; i < map->capacity; i++) {
-       if (map->entries[i] != nullptr) {
-           iterator->currentIdx = i;
-           iterator->currentEntry = map->entries[i];
-           return iterator;
-       }
-   }
-   NETSTACK_LOGI("map is empty, skip create map iterator");
-   free(iterator);
-   return nullptr;
+    if (NetstackInvalidMap(map)) {
+        NETSTACK_LOGE("create map iterator failed: invalid map");
+        return nullptr;
+    }
+    Netstack_MapIterator *iterator = (Netstack_MapIterator *)malloc(sizeof(Netstack_MapIterator));
+    if (iterator == nullptr) {
+        NETSTACK_LOGE("failed to alloc memory for map iterator");
+        return nullptr;
+    }
+    iterator->map = map;
+    for (uint32_t i = 0; i < map->capacity; i++) {
+        if (map->entries[i] != nullptr) {
+            iterator->currentIdx = i;
+            iterator->currentEntry = map->entries[i];
+            return iterator;
+        }
+    }
+    NETSTACK_LOGI("map is empty, skip create map iterator");
+    free(iterator);
+    return nullptr;
 }
 
 void Netstack_MapIterateNext(Netstack_MapIterator *iterator)
 {
-   if (iterator == nullptr || iterator->currentEntry == nullptr || NetstackInvalidMap(iterator->map) ||
-       iterator->currentIdx >= iterator->map->capacity) {
-       return;
-   }
+    if (iterator == nullptr || iterator->currentEntry == nullptr || NetstackInvalidMap(iterator->map) ||
+        iterator->currentIdx >= iterator->map->capacity) {
+        return;
+    }
 
-   if (iterator->currentEntry->next != nullptr) {
-       iterator->currentEntry = iterator->currentEntry->next;
-       return;
-   }
+    if (iterator->currentEntry->next != nullptr) {
+        iterator->currentEntry = iterator->currentEntry->next;
+        return;
+    }
 
    for (uint32_t i = iterator->currentIdx + 1; i < iterator->map->capacity; i++) {
-       if (iterator->map->entries[i] != nullptr) {
-           iterator->currentEntry = iterator->map->entries[i];
-           iterator->currentIdx = i;
-           return;
-       }
-   }
+        if (iterator->map->entries[i] != nullptr) {
+            iterator->currentEntry = iterator->map->entries[i];
+            iterator->currentIdx = i;
+            return;
+        }
+    }
 
-   iterator->currentEntry = nullptr;
-   iterator->currentIdx = iterator->map->capacity;
+    iterator->currentEntry = nullptr;
+    iterator->currentIdx = iterator->map->capacity;
 }
 
 void Netstack_DestroyMapIterator(Netstack_MapIterator *iterator)
 {
-   free(iterator);
+    free(iterator);
 }
