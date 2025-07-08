079f9f0c9d0e70f1a7d78ac4506f95a161b20406
jxw <jiexiwen@huawei.com>
2025-07-08 02:59:17 +0000

    update utils/napi_utils/src/module_template.cpp.
    Signed-off-by: jxw <jiexiwen@huawei.com>

--- utils/napi_utils/src/module_template.cpp
+++ utils/napi_utils/src/module_template.cpp
@@ -24,7 +24,9 @@
 
 #include "event_manager.h"
 #include "netstack_log.h"
+#ifndef CROSS_PLATFORM
 #include "hi_app_event_report.h"
+#endif
 
 namespace OHOS::NetStack::ModuleTemplate {
 static constexpr const int EVENT_PARAM_NUM = 2;
@@ -365,7 +367,9 @@ napi_value NewInstanceWithSharedManager(napi_env env, napi_callback_info info, c
                                         Finalizer finalizer)
 {
     NETSTACK_LOGD("create new instance for %{public}s", className.c_str());
+    #ifndef CROSS_PLATFORM
     HiAppEventReport hiAppEventReport("NetworkKit", "WebsocketConstructLocalSocketInstance");
+    #endif
     napi_value thisVal = nullptr;
     NAPI_CALL(env, napi_get_cb_info(env, info, nullptr, nullptr, &thisVal, nullptr));
 
@@ -406,7 +410,9 @@ napi_value NewInstanceWithSharedManager(napi_env env, napi_callback_info info, c
         nullptr, nullptr);
     napi_set_named_property(env, global, manager->className_.c_str(), result);
     napi_add_env_cleanup_hook(env, CleanUpWithSharedManager, reinterpret_cast<void *>(sharedManager));
+    #ifndef CROSS_PLATFORM
     hiAppEventReport.ReportSdkEvent(RESULT_SUCCESS, ERR_NONE);
+    #endif
     return result;
 }
 
