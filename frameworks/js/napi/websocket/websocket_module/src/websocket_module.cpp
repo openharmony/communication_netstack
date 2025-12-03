ba993511b26355aa655efe4b9ab7d35137dbb790
lizeo <lize49@huawei.com>
2025-12-03 19:12:27 +0800

    update: 更新文件 websocket_module.cpp
    Signed-off-by: lizeo <lize49@huawei.com>

--- frameworks/js/napi/websocket/websocket_module/src/websocket_module.cpp
+++ frameworks/js/napi/websocket/websocket_module/src/websocket_module.cpp
@@ -104,8 +104,11 @@ void WebSocketModule::FinalizeWebSocketInstance(napi_env env, void *data, void *
 {
     NETSTACK_LOGI("websocket handle is finalized");
     auto sharedManager = reinterpret_cast<std::shared_ptr<EventManager> *>(data);
+    if (sharedManager == nullptr) {
+        return;
+    }
     auto manager = *sharedManager;
-    if (manager->GetWebSocketUserData()==nullptr) {
+    if (manager->GetWebSocketUserData() == nullptr) {
         return;
     }
     auto userData = manager->GetWebSocketUserData();
