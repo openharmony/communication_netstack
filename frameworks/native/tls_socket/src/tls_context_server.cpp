49393a9ae59f5f71dab2d1d283706d48b42d4f0b
z1007019306 <zhangwenhao80@huawei.com>
2025-12-11 01:34:00 +0800

    update: 更新文件 tls_context_server.cpp
    Signed-off-by: z1007019306 <zhangwenhao80@huawei.com>

--- frameworks/native/tls_socket/src/tls_context_server.cpp
+++ frameworks/native/tls_socket/src/tls_context_server.cpp
@@ -246,7 +246,7 @@ bool TLSContextServer::SetLocalCertificate(TLSContextServer *tlsContext, const T
     if (certificate.empty()) {
         NETSTACK_LOGE("Certificate list is empty");
         return false;
-     }
+    }
 
     if (!SSL_CTX_use_certificate(tlsContext->ctx_, static_cast<X509 *>(certificate.front().handle()))) {
         NETSTACK_LOGE("Failed to set main certificate");
