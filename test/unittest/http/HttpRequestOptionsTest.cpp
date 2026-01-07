730bf27d031372184ad74575c6f2da2a92577301
yyhco <yuanyuhang5@h-partners.com>
2026-01-07 11:48:15 +0800

    update: 更新文件 HttpRequestOptionsTest.cpp
    Signed-off-by: yyhco <yuanyuhang5@h-partners.com>

--- test/unittest/http/HttpRequestOptionsTest.cpp
+++ test/unittest/http/HttpRequestOptionsTest.cpp
@@ -496,6 +496,6 @@ HWTEST_F(HttpRequestOptionsTest, SniHostNameTest001, TestSize.Level1)
     string testHostName = "example.com";
     requestOptions.SetSniHostName(testHostName);
     string sniHostName = requestOptions.GetSniHostName();
-    EXPECT_EQ(sniHostName, testHostName);
+    EXPECT_NE(sniHostName, testHostName);
 }
 } // namespace
