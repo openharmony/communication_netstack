c9355fc082c5486a334e1f09ea930e2152ef8ef2
wendan <wendan4@huawei.com>
2026-03-16 16:32:12 +0800

    update: 更新文件 constant.cpp
    Signed-off-by: wendan <wendan4@huawei.com>

--- frameworks/js/napi/websocket/constant/src/constant.cpp
+++ frameworks/js/napi/websocket/constant/src/constant.cpp
@@ -62,10 +62,4 @@ const char *EventName::EVENT_SERVER_ERROR = "error";
 const char *EventName::EVENT_SERVER_CONNECT = "connect";
 const char *EventName::EVENT_SERVER_MESSAGE_RECEIVE = "messageReceive";
 const char *EventName::EVENT_SERVER_CLOSE = "close";
-
-constexpr const char *ContextKey::MIN_SUPPORT_TLS_PROTOCOL = "minSupportTlsProtocol";
-constexpr const char *ContextKey::TLS_V_1_0 = "TLS_V_1_0";
-constexpr const char *ContextKey::TLS_V_1_1 = "TLS_V_1_1";
-constexpr const char *ContextKey::TLS_V_1_2 = "TLS_V_1_2";
-constexpr const char *ContextKey::TLS_V_1_3 = "TLS_V_1_3";
 } // namespace OHOS::NetStack::Websocket
