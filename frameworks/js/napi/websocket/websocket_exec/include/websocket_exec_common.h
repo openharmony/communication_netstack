fb08e8b59ad7989ba50f664ab6540f79d9752c29
kuruminight <xunaijie@huawei.com>
2026-06-23 22:05:03 +0800

    fix: SendData构造时vector复制范围加上LWS_SEND_BUFFER_PRE_PADDING，修复OOB导致的UAF crash
    Signed-off-by: kuruminight <xunaijie@huawei.com>

--- frameworks/js/napi/websocket/websocket_exec/include/websocket_exec_common.h
+++ frameworks/js/napi/websocket/websocket_exec/include/websocket_exec_common.h
@@ -1,4 +1,4 @@
-/*
+﻿/*
   * Copyright (c) 2025 Huawei Device Co., Ltd.
   * Licensed under the Apache License, Version 2.0 (the "License");
   * you may not use this file except in compliance with the License.
@@ -36,7 +36,7 @@ class UserData {
 public:
     struct SendData {
         SendData(void *paraData, size_t paraLength, lws_write_protocol paraProtocol)
-            : data(static_cast<uint8_t *>(paraData), static_cast<uint8_t *>(paraData) + paraLength),
+            : data(static_cast<uint8_t *>(paraData), static_cast<uint8_t *>(paraData) + LWS_SEND_BUFFER_PRE_PADDING + paraLength),
               length(paraLength), protocol(paraProtocol)
         {
         }
