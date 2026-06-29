f8ccf594a318b00e517d64f8f3ffafaafa13c4d2
lsyhh <331993722@qq.com>
2026-06-29 14:11:21 +0800

    update: 更新文件 connect_context.cpp
    Signed-off-by: lsyhh <331993722@qq.com>

--- frameworks/js/napi/socket/async_context/src/connect_context.cpp
+++ frameworks/js/napi/socket/async_context/src/connect_context.cpp
@@ -89,7 +89,7 @@ int ConnectContext::GetSocketFd() const
     if (sharedManager_ == nullptr) {
         return -1;
     }
-    return sharedManager_->GetData() ? static_cast<int>(reinterpret_cast<intptr_t>(sharedManager_->GetData())) : -1;
+    return sharedManager_->GetData() ? static_cast<int>(reinterpret_cast<uint64_t>(sharedManager_->GetData())) : -1;
 }
 
 bool ConnectContext::CheckParamsType(napi_value *params, size_t paramsCount)
