880b2df2c78ee8ec5a8824534311439e62be9008
lsyhh <331993722@qq.com>
2026-06-29 14:10:22 +0800

    update: 更新文件 bind_context.cpp
    Signed-off-by: lsyhh <331993722@qq.com>

--- frameworks/js/napi/socket/async_context/src/bind_context.cpp
+++ frameworks/js/napi/socket/async_context/src/bind_context.cpp
@@ -77,7 +77,7 @@ int BindContext::GetSocketFd() const
     if (sharedManager_ == nullptr) {
         return -1;
     }
-    return sharedManager_->GetData() ? static_cast<int>(reinterpret_cast<intptr_t>(sharedManager_->GetData())) : -1;
+    return sharedManager_->GetData() ? static_cast<int>(reinterpret_cast<uint64_t>(sharedManager_->GetData())) : -1;
 }
 
 bool BindContext::CheckParamsType(napi_value *params, size_t paramsCount)
