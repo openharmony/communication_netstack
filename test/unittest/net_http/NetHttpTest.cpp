66fa582c16b60b7a4df75bdb3aa5ca7cf3f13caf
liuleiminhw <liuleimin1@huawei.com>
2026-08-06 16:56:29 +0800

    update: 更新文件 NetHttpTest.cpp
    Signed-off-by: liuleiminhw <liuleimin1@huawei.com>

--- test/unittest/net_http/NetHttpTest.cpp
+++ test/unittest/net_http/NetHttpTest.cpp
@@ -332,7 +332,6 @@ HWTEST_F(NetHttpTest, SetOption003, TestSize.Level1)
     request->options->resumeTo = 20;
     ret = OH_Http_Request(request, callback, handler);
     EXPECT_EQ(ret, 0);
-
     free(request->options);
     OH_Http_Destroy(&request);
 }
